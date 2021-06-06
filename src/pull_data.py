45bf96f68cfbfe525f2aa9c4dfd764bb5316e9d0
Simjacs <simran.l.jackson@gmail.com>
2021-06-06 10:34:01 +0100

    refactor: add basic logging and error catching

--- src/pull_data.py
+++ src/pull_data.py
@@ -1,9 +1,6 @@
-import spotipy
-from spotipy.oauth2 import SpotifyClientCredentials
-import spotipy.util as util
-import numpy as np
 import pandas as pd
-import seaborn as sns
+import datetime
+from spotipy.exceptions import SpotifyException
 from src.utils import SpotifyConnector, resolve_relative_path
 
 pd.set_option("display.max_columns", None)
@@ -18,19 +15,22 @@ composers_path = resolve_relative_path(
 composers = pd.read_csv(composers_path)
 print(composers.head())
 
-
 features = ['danceability', 'energy', 'key', 'loudness', 'mode', 'valence', 'tempo', 'time_signature']
 rows_list = []
 timbre_rows_list = []
+composer = 24
 
-for i in range(len(composers[0:4])):
+for i in range(composer, len(composers)):
+    print(f"composer:{composer}")
+    if i % 10 == 0:
+        print(i)
+    track_ids = []
+    track_names = []
     name = f"{composers['firstnames'][i]} {composers['lastnames'][i]}"
     print(name)
     era = composers["era"][i]
     album_ids = connector.get_album_ids_from_name(name, "id")
     album_name = connector.get_album_ids_from_name(name, "name")
-    track_ids = []
-    track_names = []
     for album_id in album_ids:
         if album_id == "5OfGONHFJZD8xpz3gVBwYz":  # this is a specific bad album
             pass
@@ -43,16 +43,20 @@ for i in range(len(composers[0:4])):
             except IndexError:
                 pass
 
-for j in range(len(track_ids)):
-    # features_list = connector.get_audio_features_as_lists(track_ids[j], keys_list=features)
-    # rows_list.append([track_ids[j]] + [track_names[j]] + features_list + [era])
-    timbres = connector.get_track_timbres(track_ids[j])
-    timbre_rows_list.append([track_ids[j], timbres])
+    for j in range(len(track_ids)):
+        if j % 100 == 0:
+            print(f"track {j} out of {len(track_ids)}, composer {i} out of {len(composers)}")
+            print(str(datetime.datetime.now().time()))
+        try:
+            timbres = connector.get_track_timbres(track_ids[j])
+            timbre_rows_list.append([track_ids[j], track_names[j], era, timbres])
+        except SpotifyException:
+            pass
 
-timbres_df = pd.DataFrame(columns=["track_id", "timbres"],
-                          data=timbre_rows_list)
-timbres_path = resolve_relative_path(__file__, "data/timbres.csv", parent_levels=1)
-timbres_pickle_path = resolve_relative_path(__file__, "data/timbres.pkl", parent_levels=1)
-timbres_df.to_csv(timbres_path, index=False)
-timbres_df.to_pickle(timbres_pickle_path)
-print(timbres_df.dtypes)
+    timbres_df = pd.DataFrame(columns=["track_id", "track_name", "era", "timbres"],
+                              data=timbre_rows_list)
+
+    timbres_path = resolve_relative_path(__file__, f"data/timbres_{composer}.pkl", parent_levels=1)
+    timbres_df.to_pickle(timbres_path)
+
+    composer += 1
