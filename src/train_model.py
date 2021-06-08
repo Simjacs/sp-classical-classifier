c831c87155c8ad541f8a7ce36f2ca74bba174653
Simjacs <simran.l.jackson@gmail.com>
2021-06-08 15:38:32 +0100

    feat: specify first 2 conv net layers

--- src/train_model.py
+++ src/train_model.py
@@ -1,8 +1,29 @@
 import pandas as pd
-from src.utils import resolve_relative_path
+import numpy as np
+from keras.layers import Dense, Conv1D, Flatten, MaxPooling1D
+from keras.models import Sequential
+from src.utils import resolve_relative_path, recombine_data
 
 pd.set_option("display.max_columns", None)
 
-data_path = resolve_relative_path(__file__, "data/timbres.csv", parent_levels=1)
-data_pickle_path = resolve_relative_path(__file__, "data/timbres.pkl", parent_levels=1)
-df = pd.read_pickle(data_pickle_path)
+# read data:
+data_path = resolve_relative_path(__file__, "data", parent_levels=1)
+df = recombine_data(data_path, file_name_format="timbres_0.pkl")
+X = (df["timbres"])
+
+
+# specify model:
+input_channels = 12
+input_length = None
+model = Sequential()
+
+model.add(Conv1D(filters=20, kernel_size=10, input_shape=(input_length, input_channels), activation="relu"))
+model.add(Conv1D(filters=20, kernel_size=10, activation="relu"))
+model.add(MaxPooling1D(pool_size=4))
+
+model.add(Conv1D(filters=30, kernel_size=4, activation="relu"))
+model.add(Conv1D(filters=30, kernel_size=4, activation="relu"))
+model.add(MaxPooling1D(pool_size=8))
+
+model.add(Flatten)
+
